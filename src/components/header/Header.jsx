da5603c0decb269234b7084edb872d1aae6bddc2
mohsenmir <79303215+mohsenmir@users.noreply.github.com>
2022-07-22 15:42:53 +0430

    Navbar
    Navbar implemented

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocials from './HeaderSocials';
 const Header = () => {
   return (
     <header>
-      <div className="container header__container">
+      <section id='header' className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mohsen Mirshekari</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
@@ -19,7 +19,7 @@ const Header = () => {
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
 
-      </div>
+      </section>
     </header>
   )
 }
